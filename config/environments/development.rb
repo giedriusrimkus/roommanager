106fd96a5ba261b22b8d30775e028c4c588f87b3
giedriusrimkus <giedrius.rimkus.a@gmail.com>
2017-01-03 20:03:43 +0200

    Add account activation by email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -38,4 +37,22 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # *** Mailer ***
+  
+  config.action_mailer.raise_delivery_errors = true # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: "giedriusbox@gmail.com",
+  password: "Microlab123",
+  authentication: :login,
+  enable_starttls_auto: true
+  }
+
 end
