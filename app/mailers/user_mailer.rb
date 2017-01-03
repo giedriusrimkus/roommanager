af5c57078c1d14bdf0b0a3f18b3d837a85deed81
giedriusrimkus <giedrius.rimkus.a@gmail.com>
2017-01-03 23:15:36 +0200

    Add password reset option

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,24 +1,19 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-  #
+  # Subject can be set in I18n file at config/locales/en.yml
+  # with the lookup --> en.user_mailer.account_activation.subject
+  
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "RoomManager - Account activation"
   end
 
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
+  # Subject can be set in I18n file at config/locales/en.yml
+  # with lookup --> en.user_mailer.password_reset.subject
 
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   resources :users
@@ -12,6 +16,8 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
 
 
 
